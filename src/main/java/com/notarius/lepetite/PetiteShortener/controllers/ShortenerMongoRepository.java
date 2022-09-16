d4f1df7fa19a76cdeff474d5be934f8505567e54
Drakk3m <yunier.rene@gmail.com>
2022-09-16 14:09:13 -0400

    Added a new method to the Mongo Repository so we can check more possible edge cases

--- src/main/java/com/notarius/lepetite/PetiteShortener/controllers/ShortenerMongoRepository.java
+++ src/main/java/com/notarius/lepetite/PetiteShortener/controllers/ShortenerMongoRepository.java
@@ -3,7 +3,11 @@ package com.notarius.lepetite.PetiteShortener.controllers;
 import com.notarius.lepetite.PetiteShortener.model.DictionaryEntry;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
+import java.util.Optional;
+
 public interface ShortenerMongoRepository extends MongoRepository<DictionaryEntry, String> {
 
     public boolean existsByFullurl(String longUrl);
+
+    public Optional<DictionaryEntry> findByFullurl(String fullUrl);
 }
