82d8129bfc64f54957fa3c8f7d7b4966acc6a5fb
Drakk3m <yunier.rene@gmail.com>
2022-09-16 01:57:20 -0400

    Added JPA Annotations to the entity

--- src/main/java/com/notarius/lepetite/PetiteShortener/model/DictionaryEntry.java
+++ src/main/java/com/notarius/lepetite/PetiteShortener/model/DictionaryEntry.java
@@ -1,10 +1,22 @@
 package com.notarius.lepetite.PetiteShortener.model;
 
+import com.mongodb.lang.NonNull;
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.data.annotation.Id;
 
 public class DictionaryEntry {
-    @Getter @Setter
+
+    @Getter @Setter @NonNull
     private String full_url;
+    @Getter @Setter @Id @NonNull
     private String short_url;
+
+    @Override
+    public String toString() {
+        return "DictionaryEntry{" +
+                "full_url='" + full_url + '\'' +
+                ", short_url='" + short_url + '\'' +
+                '}';
+    }
 }
